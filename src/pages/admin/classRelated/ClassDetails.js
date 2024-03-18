4eea4720404035b966982b617023c720e1b4a0e1
BorusuVeeraRaghava <21MH1A0474@acoe.edu.in>
2024-03-18 14:45:16 +0530

    Add files via upload

---
+++ src/pages/admin/classRelated/ClassDetails.js
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom'
+import { getClassDetails, getClassStudents, getSubjectList } from "../../../redux/sclassRelated/sclassHandle";
+import {
+    Box, Container, Typography, Tab
+} from '@mui/material';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { GreenButton} from "../../../components/buttonStyles";
+const ClassDetails = () => {
+    const params = useParams()
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const { subjectsList, sclassStudents, sclassDetails, loading, error, response, getresponse } = useSelector((state) => state.sclass);
+
+    const classID = params.id
+
+    useEffect(() => {
+        dispatch(getClassDetails(classID, "Sclass"));
+        dispatch(getSubjectList(classID, "ClassSubjects"))
+        dispatch(getClassStudents(classID));
+    }, [dispatch, classID])
+
+    if (error) {
+        console.log(error)
+    }
+
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    const ClassDetailsSection = () => {
+        const numberOfSubjects = subjectsList.length;
+        const numberOfStudents = sclassStudents.length;
+
+        return (
+            <>
+                <Typography variant="h4" align="center" gutterBottom>
+                    Class Details
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                    This is Class {sclassDetails && sclassDetails.sclassName}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                    Number of Subjects: {numberOfSubjects}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                    Number of Students: {numberOfStudents}
+                </Typography>
+                {getresponse &&
+                    <GreenButton
+                        variant="contained"
+                        onClick={() => navigate("/Admin/class/addstudents/" + classID)}
+                    >
+                        Add Students
+                    </GreenButton>
+                }
+                {response &&
+                    <GreenButton
+                        variant="contained"
+                        onClick={() => navigate("/Admin/addsubject/" + classID)}
+                    >
+                        Add Subjects
+                    </GreenButton>
+                }
+            </>
+        );
+    }
+    return (
+        <>
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <Box sx={{ width: '100%', typography: 'body1', }} >
+                        <TabContext value={value}>
+                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <TabList onChange={handleChange} sx={{ position: 'fixed', width: '100%', bgcolor: 'background.paper', zIndex: 1 }}>
+                                    <Tab label="Details" value="1" />
+                                </TabList>
+                            </Box>
+                            <Container sx={{ marginTop: "3rem", marginBottom: "4rem" }}>
+                                <TabPanel value="1">
+                                    <ClassDetailsSection />
+                                </TabPanel>
+                            </Container>
+                        </TabContext>
+                    </Box>
+                </>
+            )}
+        </>
+    );
+};
+export default ClassDetails;
